4ab7f7b1383dc680b989a063aad580169a01c0dc
FCTESLA <fcamacho888@gmail.com>
2020-02-29 13:27:17 -0700

    few updates

--- script.js
+++ script.js
@@ -18,12 +18,45 @@ var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "1234567890";
 var specialCharacters = "!@#$%^&*()";
 
+
 function createPassword() {
-  passwordLength = parseInt(prompt("Please enter a length password between 8 and 128 characters"));
+  alert("Answer the following");
   lowerCase = confirm("Do you want lower cases on your password?");
   upperCases = confirm("Do you want upper cases on your password?");
   numbers = confirm("Do you want number on your password?");
   specialCharacters = confirm("Do you want special characters on your password?");
+    passwordLength = parseInt(prompt("Please enter a length password. Choose between 8 and 128 characters"))
+     if (passwordLength < 8 || passwordLength > 128){
+    prompt("Invalid number. Please enter a number between 8 and 128.");
   password = '';
+  };
+ 
+
+  if (lowerCase === true) {
+    randoLowerCase();
+  };
+
+  if (upperCases === true) {
+    randomUpperCase();
+  };
+
+  if (numbers === true) {
+    randomNumbers();
+  };
+
+  if (specialCharacters === true) {
+    randomSpecialChar();
+    return password
+  }
+  
+  
+  if (cap == false && low == false && sym == false && num == true) {
+    alert("You must choose at least one");
+  }
+    password += randGen();
+  return password;
+
+ 
 }
+
 
