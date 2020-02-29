764ec2e1cbd641b97dca0779a2d6a797e5ef4237
FCTESLA <fcamacho888@gmail.com>
2020-02-28 17:09:52 -0700

    Created variables and function to create password

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var passwordLength;
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var specialCharacters = "!@#$%^&*()";
+
+function createPassword() {
+  passwordLength = parseInt(prompt("Please enter a length password between 8 and 128 characters"));
+  lowerCase = confirm("Do you want lower cases on your password?");
+  upperCases = confirm("Do you want upper cases on your password?");
+  numbers = confirm("Do you want number on your password?");
+  specialCharacters = confirm("Do you want special characters on your password?");
+  password = '';
+}
+
