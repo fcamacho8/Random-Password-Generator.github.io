d709d18c6fec17bf211ca999d8c3206abe8bb8ef
FCTESLA <fcamacho888@gmail.com>
2020-08-24 21:49:04 -0700

    strings to single characters arrays

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-
 var generateBtn = document.querySelector("#generate");
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '`', '~', '[', '{', ']', '}', '|', ',', '.', '<', '>', '/', '?'];
+var lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
 function writePassword() {
@@ -12,13 +15,6 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 
-var passwordLength;
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "1234567890";
-var specialCharacters = "!@#$%^&*()";
-var password = "";
-var randonmPass = "";
 
 
 function createPassword() {
